5dae792f429734df04f9220f863dc4dc41ae30bd
FromSi <fromsi665@gmail.com>
2024-10-02 22:46:03 +0500

    update: repository

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,9 +13,47 @@ func main() {
 
 	signal.Notify(sigSystem, syscall.SIGINT, syscall.SIGTERM)
 
-	println("Hello")
+	route := gin.Default()
 
-	// your code ...
+	route.POST("/auth/login", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	route.POST("/auth/register", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	route.POST("/auth/refresh", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	route.POST("/auth/logout", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	route.POST("/auth/logout_all", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	route.POST("/auth/logout_device", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	route.GET("/auth/devices", func(context *gin.Context) {
+		context.JSON(http.StatusOK, map[string]string{"message": "Hello"})
+	})
+
+	route.POST("/auth/send_reset_token", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	route.POST("/auth/reset_password", func(context *gin.Context) {
+		context.Status(http.StatusContinue)
+	})
+
+	go func() {
+		route.Run(":8080")
+	}()
 
 	<-sigSystem
 }
