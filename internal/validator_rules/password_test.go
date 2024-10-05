76b87f1bf8f7bb4f33987c54534693c327ec97c8
FromSi <fromsi665@gmail.com>
2024-10-06 00:49:11 +0500

    update: repository

---
+++ internal/validator_rules/password_test.go
@@ -0,0 +1,45 @@
+package validator_rules
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type mockFieldLevel struct {
+	validator.FieldLevel
+	data string
+}
+
+func (m mockFieldLevel) Field() reflect.Value {
+	return reflect.ValueOf(m.data)
+}
+
+func TestPassword(t *testing.T) {
+	tests := []struct {
+		name     string
+		password string
+		error    bool
+	}{
+		{
+			name:     "Valid strong password",
+			password: "StrongP@ssw0rd123!",
+			error:    false,
+		},
+		{
+			name:     "Invalid weak password",
+			password: "weak",
+			error:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mock := mockFieldLevel{data: tt.password}
+
+			assert.Equal(t, Password(mock), !tt.error)
+		})
+	}
+}
